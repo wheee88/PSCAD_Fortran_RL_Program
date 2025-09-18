c9fb791141179304f393123906360b253898620a
wheee88 <wheee88@naver.com>
2025-09-18 13:20:02 +0900

    simple2

--- ddpg.f90
+++ ddpg.f90
@@ -1,4 +1,7 @@
 subroutine ddpg(state_1,reward,Done,Simu_Step_In,action_1,Simu_Step_Out)
+    use mod_network, only: network_type, network_constructor, network_output_single
+    use mod_layer, only: layer_set_activation
+    
     implicit none
     
     ! Input/Output parameters
@@ -10,6 +13,9 @@ subroutine ddpg(state_1,reward,Done,Simu_Step_In,action_1,Simu_Step_Out)
     ! Local variables
     real :: state(1), action(1)
     real :: lower_bound, upper_bound
+    type(network_type) :: actor_model
+    real, allocatable :: sampled_action(:)
+    integer :: i
     
     ! Initialize bounds
     lower_bound = -5.0
@@ -19,17 +25,22 @@ subroutine ddpg(state_1,reward,Done,Simu_Step_In,action_1,Simu_Step_Out)
     state = 0.0
     action = 0.0
     
-    ! Simple policy: just return a constant action for now
-    ! This will help us identify if the issue is with the neural network or something else
+    ! Initialize random seed
+    call random_seed()
+    
+    ! Create simple actor network
+    actor_model = network_constructor([1, 4, 1], activation='relu')
+    call layer_set_activation(actor_model%layers(3), 'tanh')
+    
+    ! Set state
     state(1) = state_1
     
-    ! Simple action selection (no neural network for now)
-    if (Simu_Step_In == 0) then
-        action(1) = 0.0  ! Start with zero action
-    else
-        ! Simple proportional control
-        action(1) = state(1) * 0.1
-    end if
+    ! Get action from network
+    allocate(sampled_action(1))
+    sampled_action = network_output_single(actor_model, state)
+    
+    ! Scale action to bounds
+    action(1) = sampled_action(1) * upper_bound
     
     ! Ensure action is within bounds
     if (action(1) < lower_bound) then
@@ -37,6 +48,9 @@ subroutine ddpg(state_1,reward,Done,Simu_Step_In,action_1,Simu_Step_Out)
     elseif (action(1) > upper_bound) then
         action(1) = upper_bound
     end if
+    
+    ! Clean up
+    deallocate(sampled_action)
     
     ! Set outputs
     action_1 = action(1)
